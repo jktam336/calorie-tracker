7db5cf98e2bf2d12d27a1ceeb891be8b810d8f7f
jktam336 <jktam336@gmail.com>
2021-11-30 15:08:16 -0800

    Made simple react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-// import MainContainer from './containers/MainContainer.jsx';
+import TotalsDisplay from './components/TotalsDisplay.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(row, square) {
+    let { turn, winner } = this.state;
+    const { rows } = this.state;
+    const squareInQuestion = rows[row][square];
+
+    if (this.state.winner) return;
+    if (squareInQuestion) return;
+
+    rows[row][square] = turn;
+    turn = turn === 'X' ? 'O' : 'X';
+    winner = checkWin(rows);
+
+    this.setState({
+      rows,
+      turn,
+      winner,
+    });
   }
 
   render() {
     return(
-      <div>Testing
-        {/* <MainContainer /> */}
+      <div>
+        <TotalsDisplay totalCalories = {this.props.totalCalories} />
       </div>
     );
   }
