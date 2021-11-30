a9e939b8e642c283a5f8a44df2b5814e62f9591c
jktam336 <jktam336@gmail.com>
2021-11-30 12:09:17 -0800

    webpack setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import MainContainer from './containers/MainContainer.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>Testing
+        {/* <MainContainer /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+    {
+      template: './index.html'
+    }),
+  ],
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /(node_modules)/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ]
+      },
+    ]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    // static: {
+    //   // directory: path.resolve(__dirname, 'build'),
+    //   directory: path.join(__dirname, '/'),
+    //   publicPath: '/build/',
+    //   // publicPath: '/',
+    // },
+    compress: true,
+    hot: true
+  },
+};
